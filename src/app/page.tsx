aa6e1a3bf3d2601b72790dcd439ff83e5807d398
Yaredi Arista <yarearista@gmail.com>
2026-01-18 00:56:11 -0600

    feat: setup main layout with responsive background and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import MainContainer from '@/components/layout/MainContainer';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Rick and Morty</h1>
-    </main>
+    <MainContainer>
+      {/* Elementos dummy para probar el Grid Areas */}
+      <div style={{ gridArea: 'search', color: 'white', border: '1px solid blue' }}>SEARCH</div>
+      <div style={{ gridArea: 'list', color: 'white', border: '1px solid green'}}>LISTA</div>
+      <div style={{ gridArea: 'detail', color: 'white', border: '1px solid yellow' }}>DETALLE</div>
+      
+    </MainContainer>
   );
 }
